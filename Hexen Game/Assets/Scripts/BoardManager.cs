fad7e95978f8131b59a69446b11c209b5e9bdb00
Ken Redant <48677852+CanAverage@users.noreply.github.com>
2021-01-17 20:03:46 +0100

    added game

---
+++ Hexen Game/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] private GameObject hexagon;
+    private float hexagonXOffset = 0.29f;
+    private float hexagonYOffset = 0.52f;
+
+    [SerializeField]private int minRows = 3;
+    [SerializeField]private int maxRows = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(hexagon)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
